68fbba57ce1318bf1ccc126271daaf8409aca1a7
dmitry <dmitry@dmitry.(none)>
2012-03-17 19:55:43 +0400

    .

--- app/models/attach.rb
+++ app/models/attach.rb
@@ -1,11 +1,10 @@
 # encoding: utf-8
-# config/locales/ru.yml
 
 class Attach < ActiveRecord::Base
   belongs_to :uploadable, :polymorphic => true
 
-  validates_presence_of :name, :message => "Выберите имя загружаемого файла"
-  validates_presence_of :upfile_file_size, :message => "Вы не выбрали файл"
+  validates_presence_of :name, :message => "can't be blank'"
+  validates_presence_of :upfile_file_size, :message => "can't be blank'"
 
   has_attached_file :upfile, :url  => "/assets/attaches/:id/:basename.:extension",
                   :path => ":rails_root/public/assets/attaches/:id/:basename.:extension"
